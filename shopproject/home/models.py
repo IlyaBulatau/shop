1c58f06ea7b956b47a607d746268f0b27c63bee7
ilya <ilyabulatau@gmail.com>
2022-12-16 18:52:48 +0300

    create new db and add slug url, make migrations

--- shopproject/home/models.py
+++ shopproject/home/models.py
@@ -3,18 +3,19 @@ from django.urls import reverse
 
 class Home(models.Model):
     title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photo/%Y/%M/%D')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return self.title
     
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = 'Home Page, car model'
@@ -22,6 +23,7 @@ class Home(models.Model):
         ordering = ['time_create', 'title']
 
 class Category(models.Model):
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     name = models.CharField(max_length=100, db_index=True, verbose_name='body type')
 
     class Meta:
--- shopproject/home/views.py
+++ shopproject/home/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404 
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
 
 menu = [{'title': 'About the site', 'url_name': 'about'},
@@ -10,10 +10,8 @@ menu = [{'title': 'About the site', 'url_name': 'about'},
 
 def home(request):
     posts = Home.objects.all()
-    cat = Category.objects.all()
     context = {
         'post': posts,
-        'cat': cat,
         'menu': menu,
         'title': 'Main Page',
         'cat_selected': 0,
@@ -32,17 +30,24 @@ def contact(request):
 def login(request):
     return HttpResponse('Sign in')
 
-def show_post(request, post_id):
-    return HttpResponse(f'Items with number id =  {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Home, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'home/post.html', context=context)
 
 def show_category(request, cat_id):
     posts = Home.objects.filter(cat_id=cat_id)
-    cat = Category.objects.all()
     if len(posts) == 0:
         raise Http404()
     context = {
         'post': posts,
-        'cat': cat,
         'menu': menu,
         'title': 'Main Page',
         'cat_selected': cat_id,
